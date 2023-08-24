4ca1d90a4916f7a75d046b6206eaeac38896679a
Low Teck Cheng <teckcheng.low@softspace.com.my>
2023-08-24 09:43:44 +0800

    Use outlinedButtons instead of Button

--- app/src/main/java/com/tclow/composecurrencyconverter/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/tclow/composecurrencyconverter/presentation/login/LoginScreen.kt
@@ -10,21 +10,29 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -35,9 +43,8 @@ import androidx.compose.ui.unit.dp
 import com.tclow.composecurrencyconverter.presentation.login.model.LoginViewModel
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(viewModel: LoginViewModel = hiltViewModel()) {
 
@@ -55,47 +62,57 @@ fun LoginScreen(viewModel: LoginViewModel = hiltViewModel()) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val layoutInformation by viewModel.layoutInformationFlow.collectAsStateWithLifecycle()
 
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        OutlinedTextField(modifier = Modifier.fillMaxWidth(0.8F),
-            value = userId,
-            onValueChange = { userId = it },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Next
-            ),
-            label = { Text("User ID") })
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(title = { Text(text = "Login") },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            ))
+    }) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues = paddingValues),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(modifier = Modifier.fillMaxWidth(0.8F),
+                value = userId,
+                onValueChange = { userId = it },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                label = { Text("User ID") })
 
-        Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedTextField(modifier = Modifier.fillMaxWidth(0.8F),
-            singleLine = true,
-            value = password,
-            visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password, imeAction = ImeAction.Next
-            ),
-            keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
-            trailingIcon = {
-                val icon = if (showPassword) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-                IconButton(onClick = {
-                    showPassword = !showPassword
-                }) {
-                    Icon(imageVector = icon, contentDescription = "")
-                }
-            })
+            OutlinedTextField(modifier = Modifier.fillMaxWidth(0.8F),
+                singleLine = true,
+                value = password,
+                visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password, imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(onNext = { keyboardController?.hide() }),
+                trailingIcon = {
+                    val icon =
+                        if (showPassword) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                    IconButton(onClick = {
+                        showPassword = !showPassword
+                    }) {
+                        Icon(imageVector = icon, contentDescription = "")
+                    }
+                })
 
-        Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.height(20.dp))
 
-        when (layoutInformation)
-        {
-            null -> { /*Loading view*/ }
-            else -> CustomRow(layoutInformation = layoutInformation!!)
+            when (layoutInformation)
+            {
+                null -> { /*Loading view*/ }
+                else -> CustomRow(layoutInformation = layoutInformation!!)
+            }
         }
     }
 }
@@ -111,12 +128,13 @@ data class LayoutInformation(
 
 @Composable
 fun CustomRow(
+    viewModel: LoginViewModel = hiltViewModel(),
     layoutInformation: LayoutInformation
 ) {
     val openUrlLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        // Handle result if needed
+    ) { _ ->
+        // No need to handle result in demo
     }
 
     Row(
@@ -124,26 +142,32 @@ fun CustomRow(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.End
     ) {
-        if (layoutInformation.layoutMeta.hasAboutApp)
-        {
-            Button(
-                modifier = Modifier.width(120.dp),
+        if (layoutInformation.layoutMeta.hasAboutApp) {
+            OutlinedButton(
                 onClick = {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(layoutInformation.layoutData.aboutAppUrl))
+                    val intent = Intent(
+                        Intent.ACTION_VIEW, Uri.parse(layoutInformation.layoutData.aboutAppUrl)
+                    )
                     openUrlLauncher.launch(intent)
-                }) {
-                Text(text = "About App")
+                }, modifier = Modifier.width(120.dp), shape = RoundedCornerShape(percent = 50)
+            ) {
+                Text(
+                    text = "About App", color = Color.Black
+                )
             }
 
             Spacer(modifier = Modifier.width(20.dp))
         }
 
-        Button(
-            modifier = Modifier.width(120.dp),
+        OutlinedButton(
             onClick = {
                 // TODO: Handle Login click
-            }) {
-            Text(text = "Login")
+                //viewModel.route()
+            }, modifier = Modifier.width(120.dp), shape = RoundedCornerShape(percent = 50)
+        ) {
+            Text(
+                text = "Login", color = Color.Black
+            )
         }
     }
 }
